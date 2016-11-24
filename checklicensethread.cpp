e5ceb4268eadc67630af0695639a199af972a3e6
VS <racroi3010@ubuntu.ubuntu-domain>
2016-11-24 20:59:12 +0900

    change

--- checklicensethread.cpp
+++ checklicensethread.cpp
@@ -5,10 +5,10 @@ CheckLicenseThread::CheckLicenseThread()
 }
 
 void CheckLicenseThread::run(){
-//    QProcess process;
-//    process.start("./" + QDir::currentPath() + "/license/pgd");
+    QProcess process;
+    process.start("./pgd");
     emit startValidateLicense();
-    //sleep(2);
+    sleep(1);
     bool rs = FaceEngineBuilder::getEngine(FaceEngineBuilder::ENGINE_NEURO)->checkLicense();
     emit validateLicense(rs);
 }
